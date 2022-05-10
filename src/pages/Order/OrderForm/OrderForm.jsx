6f0dda93dc79518c8df1981cf884f8861e1cfcb7
Topmewan <anon8733@gmail.com>
2022-05-11 01:14:57 +0500

    order page

--- src/pages/Order/OrderForm/OrderForm.jsx
+++ src/pages/Order/OrderForm/OrderForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Typography } from '../../../components';
-import { FormField } from '../../../ui-kit';
+import { FormField, IButton } from '../../../ui-kit';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
@@ -12,9 +12,18 @@ const schema = Yup.object().shape({
 		.matches(/^([^0-9]*)$/, 'Имя не должно содержать цифры')
 		.required('Укажите имя'),
 	email: Yup.string()
-		.required('Укажите ваш E-mail')
+		.required('Укажите E-mail')
 		.email('Некоректно указан E-mail,попробуйте снова'),
-	phone: Yup.string().required('Укажите ваш phone'),
+	phone: Yup.string().required('Укажите телефон'),
+	country: Yup.string()
+		.matches(/^([^0-9]*)$/, 'Страна не должна содержать цифры')
+		.required('Укажите страну'),
+	city: Yup.string()
+		.matches(/^([^0-9]*)$/, 'Город не должен содержать цифры')
+		.required('Укажите город'),
+	street: Yup.string().required('Укажите улицу'),
+	home: Yup.string().required('Укажите дом'),
+	apartment: Yup.string().required('Укажите квартиру'),
 });
 
 const normalizePhoneNumber = (val) => {
@@ -27,6 +36,11 @@ function OrderForm() {
 		name: false,
 		email: false,
 		phone: false,
+		country: false,
+		city: false,
+		street: false,
+		home: false,
+		apartment: false,
 	});
 
 	const {
@@ -103,12 +117,60 @@ function OrderForm() {
 			</div>
 			<div className={styles.data}>
 				<Typography>Адресс получателя</Typography>
+				<FormField
+					name='country'
+					register={register}
+					type='text'
+					error={errors.country && errors.country?.message}
+					placeholder='Страна'
+					isFocused={isFocused.country}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+				/>
+				<FormField
+					name='city'
+					register={register}
+					type='text'
+					error={errors.city && errors.city?.message}
+					placeholder='Город'
+					isFocused={isFocused.city}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+				/>
+				<FormField
+					name='street'
+					register={register}
+					type='text'
+					error={errors.street && errors.street?.message}
+					placeholder='Улица'
+					isFocused={isFocused.street}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+				/>
+				<FormField
+					name='home'
+					register={register}
+					type='text'
+					error={errors.home && errors.home?.message}
+					placeholder='Дом'
+					isFocused={isFocused.home}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+				/>
+				<FormField
+					name='apartment'
+					register={register}
+					type='text'
+					error={errors.apartment && errors.apartment?.message}
+					placeholder='Квартира'
+					isFocused={isFocused.apartment}
+					onBlur={handleBlur}
+					onFocus={handleFocus}
+				/>
 			</div>
-			<div className={styles.data}>
-				<Typography>Комментарий</Typography>
+			<div>
+				<IButton>Оформить заказ</IButton>
 			</div>
-
-			<button type='submit'>Sub</button>
 		</form>
 	);
 }
