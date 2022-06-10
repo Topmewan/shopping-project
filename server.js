c6779a1d4a8b58981aa1968908cfd31ce09b5126
Topmewan <anon8733@gmail.com>
2022-06-11 01:33:38 +0300

    serv

---
+++ server.js
@@ -0,0 +1,16 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
+
+const server = jsonServer.create();
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({ static: './build' });
+
+const port = process.env.PORT || 8080;
+
+server.use(middlewares);
+server.use(auth);
+server.use(cors());
+server.use(router);
+
+server.listen(port);
