e202b4bc9bfbb100841e8b5be55932d2f54c2c3c
Topmewan <anon8733@gmail.com>
2022-06-13 17:03:11 +0300

    fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,103 +1,98 @@
-import React, { useEffect, useState } from "react";
-import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
-import { ReactComponent as Logo } from "../../assets/navbar/Лого.svg";
-import { ReactComponent as Cart } from "../../assets/navbar/cart.svg";
-import { ReactComponent as Profile } from "../../assets/navbar/profile.svg";
-import { ReactComponent as Logout } from "../../assets/navbar/logout.svg";
-import { ReactComponent as Menu } from "../../assets/navbar/menu2.svg";
+import React, { useEffect, useState } from 'react';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/navbar/Лого.svg';
+import { ReactComponent as Cart } from '../../assets/navbar/cart.svg';
+import { ReactComponent as Profile } from '../../assets/navbar/profile.svg';
+import { ReactComponent as Logout } from '../../assets/navbar/logout.svg';
+import { ReactComponent as Menu } from '../../assets/navbar/menu2.svg';
 
-import { useDispatch, useSelector } from "react-redux";
-import { getTotal } from "../../feature/reducers/Cart/cart.slice";
-import { UserAuth } from "../../context/AuthContext";
+import { useDispatch, useSelector } from 'react-redux';
+import { getTotal } from '../../feature/reducers/Cart/cart.slice';
+import { UserAuth } from '../../context/AuthContext';
 
-import styles from "./Navbar.module.scss";
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const { totalQuantity, cart } = useSelector((state) => state.cart);
-  const { user, logOut } = UserAuth();
-  const [isOpen, setIsOpen] = useState(false);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const location = useLocation();
+	const { totalQuantity, cart } = useSelector((state) => state.cart);
+	const { user, logOut } = UserAuth();
+	const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    dispatch(getTotal());
-  }, [cart, dispatch]);
+	useEffect(() => {
+		dispatch(getTotal());
+	}, [cart, dispatch]);
 
-  const handleSignOut = async () => {
-    try {
-      await logOut();
-      navigate("/");
-    } catch (e) {
-      console.log(e);
-    }
-  };
+	const handleSignOut = async () => {
+		await logOut();
+	};
 
-  const handleMenu = () => {
-    setIsOpen((prev) => !prev);
-  };
+	const handleMenu = () => {
+		setIsOpen((prev) => !prev);
+	};
 
-  useEffect(() => {
-    setIsOpen(false);
-  }, [location.pathname]);
+	useEffect(() => {
+		setIsOpen(false);
+	}, [location.pathname]);
 
-  return (
-    <div className={styles.header}>
-      <div className={styles.container}>
-        <Link to="/">
-          <Logo />
-        </Link>
+	return (
+		<div className={styles.header}>
+			<div className={styles.container}>
+				<Link to='/'>
+					<Logo />
+				</Link>
 
-        <button onClick={handleMenu} className={styles.menuButton}>
-          <Menu />
-        </button>
+				<button onClick={handleMenu} className={styles.menuButton}>
+					<Menu />
+				</button>
 
-        <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Главная
-          </NavLink>
-          <NavLink
-            to="/shop"
-            className={({ isActive }) => (isActive ? styles.active : "")}
-          >
-            Магазин
-          </NavLink>
+				<nav className={`${styles.nav} ${isOpen ? styles.open : ''}`}>
+					<NavLink
+						to='/'
+						className={({ isActive }) => (isActive ? styles.active : '')}
+					>
+						Главная
+					</NavLink>
+					<NavLink
+						to='/shop'
+						className={({ isActive }) => (isActive ? styles.active : '')}
+					>
+						Магазин
+					</NavLink>
 
-          <Link to="/cart" className={styles.cart}>
-            <Cart />
-            <span>{totalQuantity}</span>
-          </Link>
+					<Link to='/cart' className={styles.cart}>
+						<Cart />
+						<span>{totalQuantity}</span>
+					</Link>
 
-          {user?.email ? (
-            <>
-              <Link to="/profile/settings">
-                <Profile />
-              </Link>
-              <Logout onClick={handleSignOut} />
-            </>
-          ) : (
-            <>
-              <NavLink
-                to="/signup"
-                className={({ isActive }) => (isActive ? styles.active : "")}
-              >
-                Регистрация
-              </NavLink>
-              <NavLink
-                to="/login"
-                className={({ isActive }) => (isActive ? styles.active : "")}
-              >
-                Войти
-              </NavLink>
-            </>
-          )}
-        </nav>
-      </div>
-    </div>
-  );
+					{user?.email ? (
+						<>
+							<Link to='/profile/settings'>
+								<Profile />
+							</Link>
+							<Logout onClick={handleSignOut} />
+						</>
+					) : (
+						<>
+							<NavLink
+								to='/signup'
+								className={({ isActive }) => (isActive ? styles.active : '')}
+							>
+								Регистрация
+							</NavLink>
+							<NavLink
+								to='/login'
+								className={({ isActive }) => (isActive ? styles.active : '')}
+							>
+								Войти
+							</NavLink>
+						</>
+					)}
+				</nav>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
